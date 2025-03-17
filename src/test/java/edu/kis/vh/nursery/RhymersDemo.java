b6852576617964e277afbdbea075eb170badb293
Witold Struminski <251227@edu.p.lodz.pl>
2025-03-17 13:10:15 +0100

    6. Zastąpienie literałów deklaracjami stałych

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -7,17 +7,21 @@ class RhymersDemo {
 
     public static void main(String[] args) {
         Rhymersfactory factory = new DefaultRhymersFactory();
+        final int max_count=15;
+        final int bound=20;
+        final int max_count2=3;
+
         
         DefaultCountingOutRhymer[] rhymers = { factory.GetStandardRhymer(), factory.GetFalseRhymer(),
                 factory.GetFIFORhymer(), factory.GetHanoiRhymer()};
         
-        for (int i = 1; i < 15; i++)
-            for (int j = 0; j < 3; j++)
+        for (int i = 1; i < max_count; i++)
+            for (int j = 0; j < max_count2; j++)
                 rhymers[j].countIn(i);
         
         java.util.Random rn = new java.util.Random();
-        for (int i = 1; i < 15; i++)
-            rhymers[3].countIn(rn.nextInt(20));
+        for (int i = 1; i < max_count; i++)
+            rhymers[max_count2].countIn(rn.nextInt(bound));
         
         for (int i = 0; i < rhymers.length; i++) {
             while (!rhymers[i].isEmpty())
@@ -26,7 +30,7 @@ class RhymersDemo {
         }
         
         System.out.println("total rejected is "
-                + ((HanoiRhymer) rhymers[3]).reportRejected());
+                + ((HanoiRhymer) rhymers[max_count2]).reportRejected());
         
     }
     
