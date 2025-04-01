888cd714ef3290d0f493f99fa19ffac63fa68a48
Wojtek Stochmialek <251226@edu.p.lodz.pl>
2025-04-01 08:03:00 +0200

    3. Scalanie zmian z format

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -5,14 +5,13 @@ public class HanoiRhymer extends defaultCountingOutRhymer {
     int totalRejected = 0;
 
     public int reportRejected() {
-
         return totalRejected;
     }
 
     public void countIn(int in) {
-    if (!callCheck() && in > peekaboo())
+        if (!callCheck() && in > peekaboo())
             totalRejected++;
-    else
+        else
             super.countIn(in);
     }
 }
